41f1a8a013ef8e13c422e88e8afb9e4591b1c633
jasonpreid-rb <jason.p.reid@outlook.com>
2026-05-17 17:41:53 +0200

    Add analytics and yards/meters toggle

--- main.jsx
+++ main.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './longest-drive-app.jsx'
+import { inject } from '@vercel/analytics'
+
+inject()
+
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
