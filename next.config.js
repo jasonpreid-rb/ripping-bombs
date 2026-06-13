ffa13841d3b85e06d96dd881919f37f8f299b8df
jasonpreid-rb <jason.p.reid@outlook.com>
2026-06-13 06:52:58 +0200

    Fix deprecated images.domains config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['flagcdn.com'] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+      },
+    ],
+  },
 }
 module.exports = nextConfig
